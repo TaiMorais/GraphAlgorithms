c9a2fdb62dfb0b2a49badde2e2d1dbd7895570ec
Tainara Marina Gonçalves Morais <74192685+TaiMorais@users.noreply.github.com>
2022-08-24 20:41:40 +0000

    ultima aula- busca

--- Graph.java
+++ Graph.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Graph {
   private int countNodes;
   private int countEdges;
@@ -54,59 +55,116 @@ public class Graph {
     return degree;
   }
 
-public int highestDegree(){
-  //returns the highest degree in the graph
-  int highest_degree = 0;
-  for (int i = 0; i < countNodes; i++){
-      if(degree(i)> highest_degree ){
+  public int highestDegree() {
+    // returns the highest degree in the graph
+    int highest_degree = 0;
+    for (int i = 0; i < countNodes; i++) {
+      if (degree(i) > highest_degree) {
         highest_degree = degree(i);
-      }      
-    }
-return highest_degree;
-}
-
-
- /* public int highestDegree(){
-  //returns the highest degree in the graph
-  int highest_degree = 0;
-  int degreeNodeI = 0;
-  for (int i = 0; i < this.adjMatrix.lenght; i++){
-      degreeNodeI = this.degree(i);
-      if(this.degree(i)> highest_degree ){
-        highest_degree = degreeNodeI; //boa pratica de prog usar o this para metodos dentro da propria classe
-      }      
-    }
-return highest_degree;
-}*/
-
-public int lowestDegree(){
-  int lowest_degree = countNodes;
-  for (int i = 0; i < countNodes; i++){
-  if(degree(i)<lowest_degree){
-        lowest_degree = degree(i);
-      }      
-    }
-return lowest_degree;
-}
-
-  public Graph complement(){
-    //returns the complement of the current graph
-    Graph g2 = new Graph(this.adjMatrix.length);
-    for (int i = 0; i < this.countNodes; i++){
-      for (int j = 0; j < this.countNodes; j++){
-        //g2.adjMatrix[i][j] = 0;
-        if (i != j && this.adjMatrix[i][j] == 0){
-          //g2.adjMatrix[i][j] = 1;
-          g2.addEdge(i, j, 1);
-        }  
       }
-    }   
+    }
+    return highest_degree;
+  }
+
+  /*
+   * public int highestDegree(){
+   * //returns the highest degree in the graph
+   * int highest_degree = 0;
+   * int degreeNodeI = 0;
+   * for (int i = 0; i < this.adjMatrix.lenght; i++){
+   * degreeNodeI = this.degree(i);
+   * if(this.degree(i)> highest_degree ){
+   * highest_degree = degreeNodeI; //boa pratica de prog usar o this para metodos
+   * dentro da propria classe
+   * }
+   * }
+   * return highest_degree;
+   * }
+   */
+
+  public int lowestDegree() {
+    int lowest_degree = countNodes;
+    for (int i = 0; i < countNodes; i++) {
+      if (degree(i) < lowest_degree) {
+        lowest_degree = degree(i);
+      }
+    }
+    return lowest_degree;
+  }
+
+  public Graph complement() {
+    // returns the complement of the current graph
+    Graph g2 = new Graph(this.adjMatrix.length);
+    for (int i = 0; i < this.countNodes; i++) {
+      for (int j = 0; j < this.countNodes; j++) {
+        // g2.adjMatrix[i][j] = 0;
+        if (i != j && this.adjMatrix[i][j] == 0) {
+          // g2.adjMatrix[i][j] = 1;
+          g2.addEdge(i, j, 1);
+        }
+      }
+    }
     return g2;
+  }
+
+  public float density() {
+    float density = this.countEdges * 2 / (this.countNodes * (this.countNodes - 1));
+    return density;
+  }
+
+  public boolean subGraph(Graph g2) {
+    // retorna true se g2 é subgrafo de this; falso caso contrário
+    if (g2.countNodes > this.countNodes || g2.countEdges > this.countEdges) {
+      return false;
+    }
+    for (int i = 0; i < g2.adjMatrix.length; i++) {
+      for (int j = 0; j < g2.adjMatrix[i].length; j++) {
+        if (g2.adjMatrix[i][j] != 0 && this.adjMatrix[i][j] == 0) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  public void addEdgeUnoriented(int u, int v, int w){
+
+    if (u < 0 || u > this.countNodes - 1 || v < 0 || v > this.countNodes - 1 || w <= 0) {
+      System.err.println("Invalid value for u, v or w!");
+      return;
+    }
+    this.adjMatrix[u][v] = w;
+    this.adjMatrix[v][u] = w;
+    this.countEdges +=2;
+    
+  }
+
+  public ArrayList<Integer> bfs(int s){
+  //public busca_largura(Graph, int s){
+  int[] desc = new int[this.countNodes];
+   ArrayList<Integer> Q = new ArrayList<>();
+    Q.add(s);
+    ArrayList<Integer> R = new ArrayList<>();
+    R.add(s);
+    desc[s] = 1;
+    //loop principal
+    while(Q.size() >0){
+      int u = Q.remove(0);
+      for(int v=0; v < this.adjMatrix[u].length; v++){
+        if(this.adjMatrix[u][v] != 0){ //verifica se v é adj a u
+          if(desc[v] == 0){// significa que essa posição ainda nao foi examinada
+          Q.add(v);
+          R.add(v);
+          desc[v] = 1;
+          }
+        }
+      }
+    }
+  return R;
+  }
+
+  public boolean connected(){
+    return this.bfs(0).size() == this.countNodes;
+  }
+
 }
-
-
-  
-     
-      
-}
-
--- Main.java
+++ Main.java
@@ -1,17 +1,34 @@
 class Main {
   public static void main(String[] args) {
-    Graph g1 = new Graph(4);
-    g1.addEdge(0, 1, 3);
-    g1.addEdge(1, 0, 3);
-    g1.addEdge(0, 3, 4);
-    g1.addEdge(3, 0, 4);
-    g1.addEdge(3, 4, 2); // aviso
-    // g1.degree(0);
-    System.out.println(g1);
-    System.out.println(g1.degree(1));
-    System.out.println("maior :" + g1.highestDegree());
-    System.out.println("menor :" + g1.lowestDegree());
-    System.out.println(g1.complement().toString());
+    Graph g1 = new Graph(9);
+    g1.addEdgeUnoriented(7, 5, 1);
+    g1.addEdgeUnoriented(7, 1, 1);
+    g1.addEdgeUnoriented(7, 2, 1);
+    g1.addEdgeUnoriented(1, 0, 1);
+    g1.addEdgeUnoriented(1, 4, 1);
+    g1.addEdgeUnoriented(2, 3, 1);
+    g1.addEdgeUnoriented(5, 6, 1);
+    g1.addEdgeUnoriented(6, 8, 1);
+    System.out.println(g1.bfs(7));
+    System.out.println(g1.connected());
 
+    /*
+     * g1.addEdge(0, 1, 3);
+     * g1.addEdge(1, 0, 3);
+     * g1.addEdge(0, 3, 4);
+     * g1.addEdge(3, 0, 4);
+     * g1.addEdge(3, 4, 2); // aviso
+     * // g1.degree(0);
+     * System.out.println(g1);
+     * System.out.println(g1.degree(1));
+     * System.out.println("maior :" + g1.highestDegree());
+     * System.out.println("menor :" + g1.lowestDegree());
+     * System.out.println(g1.complement().toString());
+     * System.out.println("Densidade" + g1.density());
+     * Graph g2 = new Graph(3);
+     * g2.addEdge(0, 1, 1);
+     * g2.addEdge(1, 0, 1);
+     * System.out.println("Subgrafo?" + g1.subGraph(g2));
+     */
   }
 }
